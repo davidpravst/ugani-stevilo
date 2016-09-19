e31eaab1891b2fa3859ca10fa6af87e601272b0d
David Pravst <david.pravst@gmail.com>
2016-09-19 18:33:18 +0200

    Moja prva sprememba

---
+++ uganistevilo.py
@@ -0,0 +1,10 @@
+# Ugani skrito stevilko
+skritastevilka = "12"
+
+while True:
+    vnesenostevilo = raw_input("Ugani skrito stevilo: ")
+    if skritastevilka == vnesenostevilo:
+        print ("Cestitam, skrito stevilo je res 12.")
+        break
+    else:
+        print ("Nisi uganil, ugani se enkrat ...")
